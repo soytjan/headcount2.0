f9cbd90a2261fdda66534b803f705fa1f094ab9f
Amanda Tjan <amanda.tjan@gmail.com>
2018-01-22 16:58:55 -0700

    Get iteration 0 tests passing

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,20 @@
-export default class DistrictRepository {
 
+
+export default class DistrictRepository {
+  constructor(data) {
+    // data in JSON
+    this.data = this.cleanData(data);
+  }
+
+  cleanData(data) {
+    return data.reduce((acc, element) => {
+      if (!acc[element.Location]) {
+        acc[element.Location] = [];
+      }
+
+      acc[element.Location].push(element);
+
+      return acc;
+    }, [])
+  }  
 }
