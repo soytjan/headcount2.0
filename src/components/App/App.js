b739d1310474bde4905c37e630e56bbf3fefa24d
Jeff Goodall <goodallhj@gmail.com>
2018-01-23 18:10:15 -0700

    Added import to the App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
+import DistrictRepository from '../../helper.js';
+import kinderData from '../../../data/kindergartners_in_full_day_program.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.district = new DistrictRepository(kinderData);
+    this.state = {
+       
+    }
+  }
+  
   render() {
     return (
       <div>Welcome To Headcount 2.0</div>
