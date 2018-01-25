748205955a33d9a186a117bb425ffed2cbe3c85b
Amanda Tjan <amanda.tjan@gmail.com>
2018-01-25 16:57:12 -0700

    Finish Search testing and test for handleSearch in App

--- src/components/CardContainer/CardContainer.test.js
+++ src/components/CardContainer/CardContainer.test.js
@@ -4,11 +4,6 @@ import CardContainer from './CardContainer';
 import { shallow, mount } from 'enzyme';
 import kinderData from '../../data/kindergartners_in_full_day_program.js';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  // ReactDOM.render(<CardContainer />, div);
-});
-
 describe('CardContainer', () => {
   it.skip('should match the snapshot', () => {
     expect(renderedComponent).toMatchSnapshot()
